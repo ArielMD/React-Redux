51a8919a32dd2d73f88742fee4381c5f550a31be
Ariel May <aemd199711@gmail.com>
2020-09-15 18:37:25 -0500

    feat: Barra de navegacion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <div className="app">
-      <h1>Hola React</h1>
+      <Navbar />
     </div>
   );
 };
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-primary">
+      <div className="navbar-brand">REDUX</div>
+
+      <a className="btn btn-danger">Nuevo &#43;</a>
+    </nav>
+  );
+};
+
+export default Navbar;
