8e6cc7383e4b8082fec98235722d2629beda8e76
Ariel May <aemd199711@gmail.com>
2020-09-15 22:07:29 -0500

    feat: store y reducer de juegos

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ import Juegos from "./Components/Juegos";
 import NuevoJuego from "./Components/NuevoJuego";
 import EditarJuego from "./Components/EditarJuego";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Juegos}></Route>
-          <Route exact path="/juegos/nuevo" component={NuevoJuego}></Route>
-          <Route
-            exact
-            path="/Juegos/editar/:id"
-            component={EditarJuego}
-          ></Route>
-        </Switch>
-      </div>
+      <Provider store={store}>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Juegos}></Route>
+            <Route exact path="/juegos/nuevo" component={NuevoJuego}></Route>
+            <Route
+              exact
+              path="/Juegos/editar/:id"
+              component={EditarJuego}
+            ></Route>
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   );
 };
